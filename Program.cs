9e10f8c509059574df0872ffb1d03d1d11107a93
Андрей Кан <1kan_andreikkkk@mail.ru>
2021-03-10 20:01:18 +0600

    Last Home 5

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _291020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public async Task<HtmlDocument> LoadPage(Uri address)
+            {
+                using (var httpResponse = await new HttpClient().GetAsync(address)
+                    .ConfigureAwait(continueOnCapturedContext: false))
+                using (var responseContent = httpResponse.Content)
+                using (var contentStream = await responseContent.ReadAsStreamAsync()
+                    .ConfigureAwait(continueOnCapturedContext: false)) 
+                    return LoadHtmlDocument(contentStream); 
+            }
+        }
+
+        private static Task<HtmlDocument> LoadHtmlDocument(Stream contentStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
